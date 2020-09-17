6ba3a1fccc153b1e6540069319519b9fa51b16d6
nick71023 <71416791+nick71023@users.noreply.github.com>
2020-09-16 23:58:36 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix = '[')
+bot = commands.Bot(command_prefix = '!')
 
 @bot.event
 async def on_ready():
@@ -11,12 +11,15 @@ async def on_ready():
 async def on_member_join(member):
     print(F'{member}join!')
     channel = bot.get_channel(755994529013694584)
-    await channel.send(F'@{member}嘿嘿嘿 歡迎加入~')    #await 呼叫斜成
+    await channel.send(F'{member}嘿嘿嘿 歡迎加入~')    #await 呼叫斜成
 
 @bot.event
 async def on_member_remove(member):
     print(F'{member}Leave!')
     channel = bot.get_channel(755994529013694584)
-    await channel.send(F"@{member}不要走~") 
-
+    await channel.send(F"{member}不要走~") 
+@bot.command()
+async def ping(ctx):
+   
+    await ctx.send(F'{round(bot.latency*1000)} ms') #round()四捨五入進位
 bot.run('NzU1OTYwMDUwMjA3NDI0Njgy.X2K4qQ.T2bNsQjeOUKCq-vcCQvGv5p78n0')
