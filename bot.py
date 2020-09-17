32426a9e7e722d6f8cf1613eb38a042359a8e709
nick71023 <71416791+nick71023@users.noreply.github.com>
2020-09-16 23:43:00 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,22 @@
-print("ddd")
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix = '[')
+
+@bot.event
+async def on_ready():
+    print(">>Bot is online<<")
+
+@bot.event
+async def on_member_join(member):
+    print(F'{member}join!')
+    channel = bot.get_channel(755994529013694584)
+    await channel.send(F'@{member}嘿嘿嘿 歡迎加入~')    #await 呼叫斜成
+
+@bot.event
+async def on_member_remove(member):
+    print(F'{member}Leave!')
+    channel = bot.get_channel(755994529013694584)
+    await channel.send(F"@{member}不要走~") 
+
+bot.run('NzU1OTYwMDUwMjA3NDI0Njgy.X2K4qQ.T2bNsQjeOUKCq-vcCQvGv5p78n0')
